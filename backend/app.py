74f2b8524f26b7143b04424727a0d5e42d3aac29
Alan Tran <alantran034@gmail.com>
2026-04-07 00:22:00 +0700

    update app for DATABASE_URL

--- backend/app.py
+++ backend/app.py
@@ -5,27 +5,52 @@ import os
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.environ.get("DB_HOST"),
-        database="postgres",
-        user="postgres",
-        password="postgres"
-    )
-    return conn
+    try:
+        conn = psycopg2.connect(
+            os.environ.get("postgresql://postgres1:FSHxbpNTO2K3TOYQ44RDUXD0XbqYL0be@dpg-d79ukln5r7bs738376ag-a/postgres1_5b72")
+        )
+        return conn
+    except Exception as e:
+        print(" Lỗi kết nối DB:", e)
+        return None
+
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Backend is running 🚀"})
+    return jsonify({
+        "message": " API Docker Cloud đang chạy!"
+    })
+
 
 @app.route("/data")
 def data():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT NOW();")
-    result = cur.fetchone()
-    cur.close()
-    conn.close()
-    return jsonify({"time": str(result[0])})
+
+    if conn is None:
+        return jsonify({
+            "status": "error",
+            "message": "Không kết nối được database"
+        })
+
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT NOW();")
+        result = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        return jsonify({
+            "status": "success",
+            "time": str(result[0])
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
